5b870aa2304e6524352688d90e89c2ff38169dad
Raphael <estagiario48399@mpf.mp.br>
2025-03-18 13:02:07 -0300

    Criar um array para armazenar os nomes

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = []
+
+
+const adicionarAmigo = () => {
+    // recebe o valor do input
+    let novoAmigo = document.getElementById('amigo').value
+    console.log(novoAmigo)
+
+    // verifica se o input está vazio
+    novoAmigo == ""
+        ?   alert("Por favor, insira um nome.")
+        :   amigos.push(novoAmigo); 
+            document.getElementById('amigo').value = ""; 
+            console.log(amigos);
+}
