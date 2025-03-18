168beba9dc2f5d77ca6e486b7462c3b1779d5dcc
Raphael <estagiario48399@mpf.mp.br>
2025-03-18 14:58:12 -0300

    Implementar uma função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -12,4 +12,35 @@ const adicionarAmigo = () => {
     console.log(amigos)
     document.getElementById('amigo').value = ""
 
+    listar()
+    
 }
+    
+    const listar = () => {
+
+        //let lista = amigos
+        const lista = document.getElementById('listaAmigos')
+        lista.innerHTML = ""
+        
+        for (let i = 0; i < amigos.length; i++) {
+            
+            const novoNomeLista = document.createElement("li")
+            novoNomeLista.innerHTML = amigos[i]
+            document.getElementById('listaAmigos').appendChild(novoNomeLista)
+            //lista = []
+            
+        }
+    }
+    
+       
+       
+      
+        
+
+    /*
+     amigos.map(amigo => {
+           let novoAmigoDaLista = document.createElement("li")
+           novoAmigoDaLista.innerHTML = amigo
+           document.getElementById('listaAmigos').appendChild(novoAmigoDaLista)
+        })
+           */
